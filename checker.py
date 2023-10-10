94244c4f0014fefb6efb2df052d437e8039f1a82
c2 <126122210+Carde2@users.noreply.github.com>
2023-10-10 22:52:40 +0300

    Обновил checker.py

--- checker.py
+++ checker.py
@@ -1,4 +1,3 @@
-#Либы
 import socket
 import re
 import sys
@@ -11,14 +10,13 @@ init()
 
 #Ввод начальных данных
 ipdomen = input(Fore.YELLOW + "Введите айпи/домен: ")
+ip = socket.gethostbyname(ipdomen)
+print(Fore.CYAN + f"Получен IP: {ip}")
 portfrom = int(input(Fore.YELLOW + "Введите порт, с которого пойдет скан (Лучше 20000): "))
 portto = int(input(Fore.YELLOW + "Введите порт, до какого пойдет скан (Лучше 65535): "))
+print(Fore.CYAN + f"Скан идёт с {portfrom} по {portto} порты")
+numbertxt = input(Fore.YELLOW + "Доп текст для подписи txt файла (servers[ваша часть].txt): ")
 
-
-#Логика перевода домена в айпи
-    #Получен домен
-ip = socket.gethostbyname(ipdomen)
-print(Fore.CYAN + f"Получен IP: {ip}\nСкан идёт с {portfrom} по {portto} порты")
 #Логика скана
 def check_port(port):
     cont = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,20 +30,25 @@ def check_port(port):
             clear_motd = re.sub(r'§.', '', motd)
             print(Fore.GREEN + f"Сервер {ip}:{str(port)} существует")
             print(Fore.WHITE + f"{clear_motd}\n")
-        except Exception as e:
-            print(Fore.RED + f"Ошибка при получении MOTD: {e}")
-        with open("servers.txt", "a") as file:
-            file.write(f"{ip}:{str(port)}\nMOTD: {clear_motd}\n\n")
+        except Exception as error:
+            print(Fore.RED + f"Сервер {ip}:{str(port)} Есть\n Но произошла ошибка при получении MOTD: \n" + Fore.LIGHTRED_EX + f"{error}\n")
+            with open(f"servers{numbertxt}.txt", "a") as file:
+                file.write(f"{ip}:{str(port)}\nНе удалось получить MOTD, возможно сервер недействителен\n\n")
+        with open(f"servers{numbertxt}.txt", "a") as file:
+            if clear_motd == "A Minecraft Server":
+                file.write(f"{ip}:{str(port)}\n Velocity or Bungee\n\n")
+            else:
+                file.write(f"{ip}:{str(port)}\nMOTD: {clear_motd}\n\n")
     except socket.error:
         print(Fore.RED + f"Сервер {ip}:{str(port)} не существует")
     finally:
         cont.close()
-#Получен айпи
 with ThreadPoolExecutor() as executor:
-    executor.map(check_port, range(portfrom, portto))
+    executor.map(check_port, range(portfrom, portto+1))
+
 
 #Выход
 input("Enter для выхода!")
+#ЭТО МОЖНО ИСПОЛЬЗОВАТЬ ДЛЯ ТЕСТА!
 #Айпи для теста              65.21.127.190
 #Домен для теста             d19.gamely.pro
-#Всё работает
